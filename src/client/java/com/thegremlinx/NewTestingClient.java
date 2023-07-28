8a970d6781693bbe28d1233e65adba1300d7a3e9
devonk15 <kaylordevon5@gmail.com>
2023-07-28 00:11:34 -0400

    done and dusted

---
+++ src/client/java/com/thegremlinx/NewTestingClient.java
@@ -0,0 +1,10 @@
+package com.thegremlinx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewTestingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
