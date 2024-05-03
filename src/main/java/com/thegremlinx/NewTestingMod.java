104d339600713ce58f4c7455cc0629432b647cc5
devonk15 <kaylordevon5@gmail.com>
2024-05-03 16:11:25 -0400

    done and dusted

--- src/main/java/com/thegremlinx/NewTestingMod.java
+++ src/main/java/com/thegremlinx/NewTestingMod.java
@@ -2,7 +2,6 @@ package com.thegremlinx;
 
 import com.thegremlinx.block.ModBlocks;
 import com.thegremlinx.item.ModItems;
-import com.thegremlinx.item.ModItemGroup;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,14 +12,11 @@ public class NewTestingMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	
 	//an instance of our new item
+	
 	@Override
 	public void onInitialize() {
 		LOGGER.info("The mod: "+ MOD_ID +" has been initialized.");
-		ModItemGroup.registerModItemGroup();
-		ModItemGroup.intialize();
-		ModItems.registerModItems();
 		ModItems.initialize();
-		ModBlocks.registerModBlocks();
 		ModBlocks.initialize();
 	}
 }
--- src/main/java/com/thegremlinx/item/ModItemGroup.java
+++ src/main/java/com/thegremlinx/item/ModItemGroup.java
@@ -1,35 +1,12 @@
 package com.thegremlinx.item;
 
 import com.thegremlinx.NewTestingMod;
-import com.thegremlinx.block.ModBlocks;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-
 public class ModItemGroup {
-    private static final ItemGroup UNKNOWN = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(NewTestingMod.MOD_ID, "unknown"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemGroup.unknown-mod.unknown"))
-                    .icon(() -> new ItemStack(ModItems.UNKNOWN_ITEM)).entries((displayContext, entries) -> {
-                        entries.add(ModItems.UNKNOWN_ITEM);
-                        entries.add(ModBlocks.UNKNOWN_BLOCK);
-                    }).build());
-    private static final ItemGroup UNKNOWN2 = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(NewTestingMod.MOD_ID, "unknown2"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemGroup.unknown-mod.unknown2"))
-                    .icon(() -> new ItemStack(ModItems.UNKNOWN_ITEM)).entries((displayContext, entries) -> {
-                        entries.add(Items.NETHERITE_BLOCK);
-                    }).build());
-    public static void registerModItemGroup () {
-        NewTestingMod.LOGGER.info("Registering Mod Item Group 1 for: "+ NewTestingMod.MOD_ID);
-    }
-    public static void intialize () {
-        NewTestingMod.LOGGER.info("Mod Item Group for: "+ NewTestingMod.MOD_ID +" has been initialized.");
-    }
+    public static final ItemGroup UNKNOWN = FabricItemGroupBuilder.build(
+            new Identifier(NewTestingMod.MOD_ID, "unknown"), () -> new ItemStack(ModItems.UNKNOWN_ITEM));
 }
