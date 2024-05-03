7112239118e642323b7fa078378efc4648b00109
devonk15 <kaylordevon5@gmail.com>
2024-05-03 16:36:10 -0400

    done and dusted

--- src/main/java/com/thegremlinx/item/ModItems.java
+++ src/main/java/com/thegremlinx/item/ModItems.java
@@ -3,21 +3,18 @@ package com.thegremlinx.item;
 import com.thegremlinx.NewTestingMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 
 public class ModItems {
     public static final Item UNKNOWN_ITEM = registerItem("unknown_item",
-            new Item(new FabricItemSettings().group(ModItemGroup.UNKNOWN).maxCount(1)));
-
+            new Item(new FabricItemSettings().maxCount(1)));
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registry.ITEM, new Identifier(NewTestingMod.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, new Identifier(NewTestingMod.MOD_ID, name), item);
     }
     public static void initialize() {
-        NewTestingMod.LOGGER.info("Mod Items for: "+NewTestingMod.MOD_ID+" have been registered.");
         NewTestingMod.LOGGER.info("Registering mod items for " + NewTestingMod.MOD_ID);
+        NewTestingMod.LOGGER.info("Mod Items for: "+NewTestingMod.MOD_ID+" have been registered.");
     }
-
-
 }
