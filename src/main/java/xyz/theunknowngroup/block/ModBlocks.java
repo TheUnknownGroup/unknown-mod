aefc1e437311b38d2b6f8fac46066ed93861a181
devonk15 <94760159+devonk15@users.noreply.github.com>
2026-06-23 13:57:36 -0400

    that was smt... [m] 1.21.2+ update

--- src/main/java/xyz/theunknowngroup/block/ModBlocks.java
+++ src/main/java/xyz/theunknowngroup/block/ModBlocks.java
@@ -1,29 +1,32 @@
 package xyz.theunknowngroup.block;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
 import xyz.theunknowngroup.UnknownMod;
+
+import java.util.function.Function;
 
 public class ModBlocks {
     public static String MOD_ID = UnknownMod.MOD_ID;
     public static String MOD_NAME = UnknownMod.MOD_NAME;
     public static final Block UNKNOWN_BLOCK = registerBlock("unknown_block",
-            new Block(Block.Settings.copy(Blocks.STONE)));
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, name), block);
+            Block::new, Block.Settings.copy(Blocks.STONE));
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> fac, AbstractBlock.Settings block) {
+        final Identifier id = Identifier.of(MOD_ID, name);
+        final RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, id);
+        final Block blocks = Blocks.register(key, fac, block);
+        Items.register(blocks);
+        return blocks;
     }
-
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name),
-                new BlockItem(block, new Item.Settings().maxCount(64)));
-    }
-
     public static void registerModBlocks() {
         UnknownMod.LOGGER.info("[{}] Registering mod blocks for {}", MOD_NAME, MOD_ID);
     }
--- src/main/java/xyz/theunknowngroup/item/ModItems.java
+++ src/main/java/xyz/theunknowngroup/item/ModItems.java
@@ -1,19 +1,25 @@
 package xyz.theunknowngroup.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
 import xyz.theunknowngroup.UnknownMod;
+
+import java.util.function.Function;
 
 public class ModItems {
     public static String MOD_ID = UnknownMod.MOD_ID;
     public static String MOD_NAME = UnknownMod.MOD_NAME;
     public static final Item UNKNOWN_ITEM = registerItem("unknown_item",
-            new Item(new Item.Settings().maxCount(1)));
+            Item::new, new Item.Settings().maxCount(1));
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name), item);
+    public static Item registerItem(String name, Function<Item.Settings, Item> fac, Item.Settings item) {
+        final RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
+        return Items.register(key, fac, item);
     }
 
     public static void registerModItems() {
