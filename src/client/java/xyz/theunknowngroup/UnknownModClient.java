0f9e8f17e0a3665fd0f1577ee476943a46d249d1
devonk15 <94760159+devonk15@users.noreply.github.com>
2026-06-28 12:45:07 -0400

    Delete src directory [skip]

--- src/client/java/xyz/theunknowngroup/UnknownModClient.java
+++
@@ -1,9 +0,0 @@
-package xyz.theunknowngroup;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class UnknownModClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
--- src/main/resources/assets/unknown-mod/data/addblocks/functions/blocks.mcfunction
+++
@@ -1,2 +0,0 @@
-execute as @a at @s run give @s unknown-mod:unknown_item
-execute as @a at @s run give @s unknown-mod:unknown_block
